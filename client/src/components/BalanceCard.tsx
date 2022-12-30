a493bd23f640164844207d3616bd62cbfbd80bb5
unknown <max@maxflores.dev>
2022-12-30 08:56:47 -0600

    Balance Card

--- client/src/components/BalanceCard.tsx
+++ client/src/components/BalanceCard.tsx
@@ -1,4 +1,3 @@
-
 import { useSelector } from 'react-redux'
 
 const BalanceCard = () => {
@@ -7,36 +6,43 @@ const BalanceCard = () => {
 
     let sum = 0
 
-
     if (expenses?.length > 0) {
         for (let i = 0; expenses?.length > i; i++) {
             sum = sum + expenses[i].amount
         }
     }
 
-    const total = current.balance - sum
+    const total = current.balance - sum    
 
     return (
         <div className="row">
             <div className="balance">
-                <div>
+                <div className='card-amount mod'>
                     <h4>Total Spent</h4>
                     <h2>L {sum.toFixed(2)}</h2>
                 </div>
-                <div>
+                <div className='card-amount'>
                     <h4>Remaining Balance</h4>
                     <h2>L {total.toFixed(2)}</h2>
                 </div>
             </div>
-            <div className="balance">
-                <div>
-                    <h4>Bank Balance</h4>
-                    <h2>L {current?.balance?.toFixed(2)}</h2>
+            {current?.balance !== 0 ? (
+                <div className="balance mod">
+                    <div className='card-amount mod2'>
+                        <h4>Bank Balance</h4>
+                        <h2>L {current?.balance?.toFixed(2)}</h2>
+                    </div>
+                    <div>
+                        <button className='btn2 mod'>Update Balance</button>
+                    </div>
                 </div>
-                <div>
-                    <button className='btn'>Update Balance</button>
+            ) : (
+                <div className="balance mod">
+                    <h3>Bank Balance</h3>
+                    <button className='btn'>Add Balance</button>
                 </div>
-            </div>
+
+            )}
         </div>
     )
 }
